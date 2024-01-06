5c6d3460307f37945314283d644010e394e3f044
Subeen9 <subin.bista@selu.edu>
2024-01-05 22:41:12 -0600

    Homepage image edited and icons added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,102 @@
 import React from "react";
-import { Grid, Typography,Button,styled } from "@mui/material";
-import Subin from '../assests/subin2.png'
-import Cloud from '../assests/cloud.png'
-import './App.css'
-export const Home = ()=>{
-    return(
-        <>
-        <Grid container spacing={2} style={{backgroundColor: '#ADD8E6'}}>
-        <Grid item xs={8} style={{marginTop:'90px'}}>
-            <img src={Cloud} style={{animation: 'float 5s ease-in-out infinite'}} className="float" />
-            <Typography variant="h4" gutterBottom style={{color: '#191970'}}>
-                Hey There
-            </Typography>
-            <Typography variant="h2" gutterBottom style={{color: '#191970'}}>
-                I am Subin Bista
-            </Typography>
-            <Typography variant="h4" gutterBottom style={{color: '#191970'}}>
-                Computer Science Student interested in Full Stack development 
-            </Typography>
-            <Button variant="contained" style={{backgroundColor: '#191970', color: '#FFFFFF'}}>
-                See my Projects
-            </Button>
-            <img src={Cloud} style={{animation: 'float 5s ease-in-out infinite'}} className="float" />
-        
-        </Grid>
-        <Grid item xs={4} >
+import { Grid, Typography, Button, Stack } from "@mui/material";
+import Subin from "../assests/subin2.png";
+import Cloud from "../assests/cloud.png";
+import "./App.css";
+import styled from "@emotion/styled";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
-            <img src={Subin} style={{marginTop:'2px', width: '100%', height: 'auto', marginRight:'90px'}} alt="profile image"/>
-        </Grid>
-        </Grid>
-        </>
-    )
-}
+const CenterContent = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  color: "#191970",
+  marginLeft: "120px",
+});
+const IconContainer = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  border: "1px solid #191970",
+  borderRadius: "50%",
+  width: "50px",
+  height: "50px",
+  margin: "10px",
+});
+const HighlightedText = styled(Typography)({
+  fontSize: "4rem",
+  color: "#191970",
+  fontWeight: "bold",
+});
+const HighlightedButton = styled(Button)({
+  fontSize: "1.5rem",
+  padding: "10px 20px",
+  border: "2px solid #191970",
+  backgroundColor: "#ADD8E6",
+});
 
-// Add this to your CSS
+export const Home = () => {
+  return (
+    <>
+      <Grid container spacing={2} style={{ backgroundColor: "#ADD8E6" }}>
+        <Grid item xs={8} style={{ marginTop: "90px" }}>
+          <img
+            src={Cloud}
+            style={{ animation: "float 5s ease-in-out infinite" }}
+            className="float"
+            alt="cloud"
+          />
+          <CenterContent>
+            <HighlightedText variant="h4">I am Subin Bista</HighlightedText>
+          </CenterContent>
+          <br />
+          <br />
+          <CenterContent>
+            <Typography variant="h6">
+              Computer Science Student interested in Full Stack development
+            </Typography>
+          </CenterContent>
+          <br />
+          <br />
+          <CenterContent>
+            <HighlightedButton
+              variant="contained"
+              style={{ backgroundColor: "#191970", color: "#FFFFFF" }}
+            >
+              See my Projects
+            </HighlightedButton>
+          </CenterContent>
+          <Grid xs={0.6}>
+            <Stack spacing={2}>
+              <IconContainer>
+                <LinkedInIcon fontSize="large" />
+              </IconContainer>
+              <IconContainer>
+                <GitHubIcon fontSize="large" />
+              </IconContainer>
+              <IconContainer>
+                <InstagramIcon fontSize="large" />
+              </IconContainer>
 
+              <br />
+            </Stack>
+          </Grid>
+        </Grid>
+        <Grid item xs={4}>
+          {/* <img src={Cloud} style={{animation: 'float 5s ease-in-out infinite'}} className="float" alt="profile"/> */}
+          <img
+            src={Subin}
+            style={{
+              marginTop: "2px",
+              width: "100%",
+              height: "auto",
+              marginRight: "90px",
+            }}
+            alt="profile image"
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
